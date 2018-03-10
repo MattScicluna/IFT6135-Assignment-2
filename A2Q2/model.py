39cb17e7a0191946af97d2e229395dbe5503729c
mattscicluna <mattcscicluna@gmail.com>
2018-03-10 12:43:24 -0500

    added save model capability

--- A2Q2/model.py
+++ A2Q2/model.py
@@ -1,50 +1,66 @@
-import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.functional as F
 
-class imgCNN(nn.Module):
+
+class imCNN3(nn.Module):
+    """Convnet Classifier"""
+
     def __init__(self):
-        super(imgCNN, self).__init__()
-        self.pool = nn.MaxPool2d(2, 2)
+        super(imCNN3, self).__init__()
+        self.conv = nn.Sequential(
+            # Layer 1
+            nn.Conv2d(in_channels=3, out_channels=18, kernel_size=(3, 3), padding=1),
+            #nn.Dropout(p=0.5),
+            nn.BatchNorm2d(18),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
 
-        self.conv1 = nn.Conv2d(3, 20, 3)
-        torch.nn.init.xavier_uniform(self.conv1.weight)
-        self.bn1 = nn.BatchNorm2d(20)
+            # Layer 2
+            nn.Conv2d(in_channels=18, out_channels=36, kernel_size=(3, 3), padding=1),
+            #nn.Dropout(p=0.5),
+            nn.BatchNorm2d(36),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
 
-        self.conv2 = nn.Conv2d(20, 20, 3)
-        torch.nn.init.xavier_uniform(self.conv2.weight)
+            # Layer 3
+            nn.Conv2d(in_channels=36, out_channels=72, kernel_size=(3, 3), padding=1),
+            # nn.Dropout(p=0.5),
+            nn.BatchNorm2d(72),
+            nn.ReLU(),
 
-        self.conv3 = nn.Conv2d(20, 40, 3)
-        self.bn2 = nn.BatchNorm2d(40)
-        torch.nn.init.xavier_uniform(self.conv3.weight)
-        self.conv4 = nn.Conv2d(40, 40, 3)
+            # Layer 4
+            nn.Conv2d(in_channels=72, out_channels=72, kernel_size=(3, 3), padding=1),
+            # nn.Dropout(p=0.5),
+            nn.BatchNorm2d(72),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
 
-        self.conv5 = nn.Conv2d(40, 80, 3)
-        self.bn3 = nn.BatchNorm2d(80)
-        self.conv6 = nn.Conv2d(80, 80, 3)
-        self.conv7 = nn.Conv2d(80, 80, 3)
+            # Layer 5
+            nn.Conv2d(in_channels=72, out_channels=144, kernel_size=(3, 3), padding=1),
+            # nn.Dropout(p=0.5),
+            nn.BatchNorm2d(144),
+            nn.ReLU(),
 
-        self.conv8 = nn.Conv2d(80, 160, 3)
-        self.bn4 = nn.BatchNorm2d(160)
+            # Layer 6
+            nn.Conv2d(in_channels=144, out_channels=144, kernel_size=(3, 3), padding=1),
+            # nn.Dropout(p=0.5),
+            nn.BatchNorm2d(144),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+        )
 
-        self.fc1 = nn.Linear(160, 160)
-        self.bn5 = nn.BatchNorm1d(160)
-        self.fc2 = nn.Linear(160, 50)
-        self.bn6 = nn.BatchNorm1d(50)
-        self.fc3 = nn.Linear(50, 2)
+        # Linear units
+        self.linear = nn.Sequential(
+            nn.Linear(144 * 4 * 4, 144 * 4 * 4),
+            nn.BatchNorm1d(144 * 4 * 4),
+            nn.ReLU(),
+            nn.Linear(144 * 4 * 4, 500),
+            nn.BatchNorm1d(500),
+            nn.ReLU(),
+            nn.Linear(500, 2)
+        )
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = self.pool(F.relu(self.bn1(self.conv2(x))))
-        x = F.relu(self.bn2(self.conv3(x)))
-        x = self.pool(F.relu(self.bn2(self.conv4(x))))
-        x = F.relu(self.bn3(self.conv5(x)))
-        x = F.relu(self.bn3(self.conv6(x)))
-        x = self.pool(F.relu(self.bn3(self.conv7(x))))
-        x = F.relu(self.bn4(self.conv8(x)))
-        x = x.view(-1, 160)
-        x = F.relu(self.bn5(self.fc1(x)))
-        x = F.relu(self.bn6(self.fc2(x)))
-        x = self.fc3(x)
+        x = self.conv(x)
+        x = x.view(-1, 144*4*4)
+        x = self.linear(x)
         return x
--- A2Q2/train.py
+++ A2Q2/train.py
@@ -14,7 +14,7 @@ import torch
 import torch.nn as nn
 
 # local imports
-from model import imgCNN
+from model import imCNN3 as imgCNN
 
 
 def main():
@@ -25,9 +25,9 @@ def main():
     argparser.add_argument('--test_set', type=str, required=True)
     argparser.add_argument('--learning_rate', type=float, default=0.01)
     argparser.add_argument('--n_epochs', type=int, default=100)
-    argparser.add_argument('--batch_size', type=int, default=100)
-    argparser.add_argument('--weight_decay', type=int, default=5e-3)
-    argparser.add_argument('--momentum', type=int, default=0.9)
+    argparser.add_argument('--batch_size', type=int, default=50)
+    #argparser.add_argument('--weight_decay', type=int, default=5e-3)
+    #argparser.add_argument('--momentum', type=int, default=0.9)
     argparser.add_argument('--num_workers', type=int, default=8)
     argparser.add_argument('--model_file', type=str, default='None')
     argparser.add_argument('--cuda', action='store_true')
@@ -38,8 +38,6 @@ def main():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         #  Pytorch ImageNet implementation
     ])
-
-
 
     train_dataset = datasets.ImageFolder(root=os.path.join(os.getcwd(), args.train_set),
                                          transform=data_transform)
@@ -53,31 +51,49 @@ def main():
                                   drop_last=True)
     test_dataset = datasets.ImageFolder(root=os.path.join(os.getcwd(), args.test_set),
                                         transform=data_transform)
-    test_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size,
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size,
                                  shuffle=False, num_workers=args.num_workers,
                                  drop_last=True)
+
+    train_acc_vec, valid_acc_vec, train_loss_vec, valid_loss_vec = [], [], [], []
 
     if args.model_file == 'None':
         epoch_from = 1
         decoder = imgCNN()
+
+        # Optimizer
+        optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate)
+
+        # optimizer = torch.optim.SGD(decoder.parameters(), lr=args.learning_rate,
+        #                            momentum=args.momentum, weight_decay=args.weight_decay)
+
     else:
-        epoch_from = 1
+        model = torch.load(args.model_file)
+        epoch_from = model['epoch']
+        state_dict = model['state_dict']
         decoder = imgCNN()
+        decoder.load_state_dict(state_dict)
+        optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate)
+        optimizer_state = model['optimizer']
+        optimizer.load_state_dict(optimizer_state)
+
+        train_acc_vec = model['train_acc_vec']
+        valid_acc_vec = model['valid_acc_vec']
+        train_loss_vec = model['train_loss_vec']
+        valid_loss_vec = model['valid_loss_vec']
+        print('model successfully loaded! Resuming training...')
 
     if args.cuda:
         print('running with GPU...')
         decoder.cuda()
 
-    # Loss and Optimizer
+    # Loss
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(decoder.parameters(), lr=args.learning_rate,
-                                momentum=args.momentum, weight_decay=args.weight_decay)
-
-    #optimizer = torch.optim.Adam(decoder.parameters(), lr=args.learning_rate) # use with batch=50,lr=0.01
 
     try:
         print('Training for maximum {} epochs...'.format(args.n_epochs))
         for epoch in range(epoch_from, args.n_epochs + 1):
+
             num_samples_t, training_loss, train_acc = 0, 0, 0
             decoder.train()
             for (input, target) in train_dataloader:
@@ -98,7 +114,7 @@ def main():
                 optimizer.step()
                 num_samples_t += args.batch_size
 
-            num_samples_v, valid_acc = 0, 0
+            num_samples_v, valid_acc, valid_loss = 0, 0, 0
             decoder.eval()
             for (input, target) in valid_dataloader:
                 input = Variable(input)
@@ -108,18 +124,81 @@ def main():
                     target = target.cuda()
 
                 outputs = decoder(input)
+                loss = criterion(outputs, target)
+                valid_loss += loss.data[0]
                 output_vals = outputs.max(1)[1].type_as(target)
                 valid_acc += output_vals.eq(target).sum().data[0]
                 num_samples_v += args.batch_size
 
             pcnt = epoch / args.n_epochs * 100
-            log = ('epoch #{} ({:.1f}%) - training loss {:.2f} - training acc: {:.2f}% - valid acc {:.2f}%')
-            print(log.format(epoch, pcnt, training_loss, train_acc/num_samples_t * 100, valid_acc/num_samples_v * 100))
 
+            #  Save results
+            train_acc_vec.append(train_acc/num_samples_t * 100)
+            train_loss_vec.append(training_loss/num_samples_t)
+            valid_acc_vec.append(valid_acc/num_samples_v * 100)
+            valid_loss_vec.append(valid_loss/num_samples_v)
+
+            #  Display current results
+            log = ('epoch #{} ({:.1f}%) - training loss {:.4f} - training acc: {:.2f}% - valid acc {:.2f}%')
+            print(log.format(epoch,
+                             pcnt,
+                             training_loss/num_samples_t,
+                             train_acc/num_samples_t * 100,
+                             valid_acc/num_samples_v * 100))
 
     except KeyboardInterrupt:
         print("Saving before quit...")
+        state = {
+            'epoch': epoch,
+            'state_dict': decoder.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'train_acc_vec': train_acc_vec,
+            'valid_acc_vec': valid_acc_vec,
+            'train_loss_vec': train_loss_vec,
+            'valid_loss_vec': valid_loss_vec
+        }
+        torch.save(state, 'models/epoch-{0}-trainloss-{1}'.format(epoch, train_acc_vec[-1]))
 
+    # Evaluate test set
+    decoder.eval()
+    test_acc, test_loss, num_samples_test = 0, 0, 0
+    for (input, target) in test_dataloader:
+        input = Variable(input)
+        target = Variable(target)
+        if args.cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        outputs = decoder(input)
+        loss = criterion(outputs, target)
+        test_loss += loss.data[0]
+        output_vals = outputs.max(1)[1].type_as(target)
+        test_acc += output_vals.eq(target).sum().data[0]
+        num_samples_test += args.batch_size
+    log = ('test loss {:.4f} - test acc: {:.2f}%')
+    print(log.format(test_loss / num_samples_test, test_acc / num_samples_test * 100))
+
+    plt.figure()
+    plt.title('Accuracy per Epoch')
+    plt.plot(train_acc_vec, color='red', label='training set')
+    plt.plot(valid_acc_vec, color='blue', label='validation set')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='upper right')
+    plt.savefig('figs/acc')
+    plt.axis([0, 100, 0, 100])
+    # plt.show()
+
+    plt.figure()
+    plt.title('Loss per Epoch')
+    plt.plot(train_loss_vec, color='red', label='training set')
+    plt.plot(valid_loss_vec, color='blue', label='validation set')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc='upper right')
+    plt.savefig('figs/loss')
+    plt.axis([0, 100, 0, 100])
+    #plt.show()
 
 if __name__ == '__main__':
     main()
